dffb6f55c77b927e14093be7517e2ab73c8af93d
Michael Huang <mxh2@cornell.edu>
2014-10-11 23:48:54 -0400

    new c

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include <pebble.h>
+static Window *s_main_window;
+static TextLayer *s_text_layer;
+
+static void main_window_load(Window *window){
+  s_text_layer = text_layer_create(GRect(0,5,144,50));
+  text_layer_set_background_color(s_text_layer,GColorClear);
+  text_layer_set_text_color(s_text_layer,GColorBlack);
+  
+  text_layer_set_font(s_text_layer,fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  layer_add_child(window_get_root_layer(window),text_layer_get_layer(s_text_layer));
+  
+}
+
+static void main_window_unload(Window *window){
+  text_layer_destroy(s_text_layer);
+}
+
+static void init(){
+  s_main_window = window_create();
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+  window_stack_push(s_main_window,true);
+}
+
+static void deinit(){
+  window_destroy(s_main_window);
+}
+int main(void){
+  init();
+  app_event_loop();
+  deinit();
+}
